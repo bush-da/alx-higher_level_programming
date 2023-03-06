eb8e19fe3ef962c38b98f5a6d7b1e81a8113edfe
bush-da <danielsevani12@gmail.com>
2023-03-06 16:03:28 -0500

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list have cycle
+ * @list: linked list to check
+ * Return: 0 if doesn't have cycle, 1 if it have
+ */
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *flag = list;
+	listint_t *travel = list;
+
+	if (!flag)
+	{
+		return 0;
+	}
+
+	travel = travel->next;
+	while (travel)
+	{
+		if (travel == flag)
+			return 1;
+		travel = travel->next;
+	}
+	return 0;
+}
