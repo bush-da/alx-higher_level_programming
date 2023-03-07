486d2a171f7ffe6a0631916c3f077e0fa7168b66
bush-da <danielsevani12@gmail.com>
2023-03-07 12:39:15 -0500

    print number from 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 endd = ", "
 for i in range(100):
     if i <= 9:
-        print("{}{}".format(0,i), end=endd)
+        print("{}{}".format(0, i), end=endd)
     else:
         if i == 99:
             endd = "\n"
