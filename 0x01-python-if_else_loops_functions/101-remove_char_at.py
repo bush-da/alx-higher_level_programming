853846aa0c94172c4e1929056327410f0b5eb86f
bush-da <danielsevani12@gmail.com>
2023-03-07 15:45:31 -0500

    remove char at n pos

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            new += str[i]
+    return new
