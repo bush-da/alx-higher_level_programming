2e39241e71883a86ead70f6a982313db32802e01
bush-da <danielsevani12@gmail.com>
2023-03-09 09:25:51 -0500

    add infinitly command line argument

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+
+    print("{}".format(total))
