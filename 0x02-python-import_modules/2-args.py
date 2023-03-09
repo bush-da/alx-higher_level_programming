c0d3c91c461cc2591b1f3630e175795e3aef9518
bush-da <danielsevani12@gmail.com>
2023-03-09 09:14:18 -0500

    command line arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 
 if __name__ == '__main__':
     import sys
+
     if (len(sys.argv) == 1):
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1), end="\n")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1), end="\n")
         for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]), end="\n")
