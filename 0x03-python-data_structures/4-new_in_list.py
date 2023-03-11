351d1dd8ff21467e7bea20ef21a228a8fa08e9f5
bush-da <danielsevani12@gmail.com>
2023-03-11 13:03:49 -0500

    replace value in a list without changing the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or len(my_list) < idx:
+    if idx < 0 or len(my_list) <= idx:
         return my_list
 
     my_copy = my_list[:]
