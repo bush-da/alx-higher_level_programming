71c07a9bb7d455f97ab04aa2aa63f3f1963fbf3c
bush-da <danielsevani12@gmail.com>
2023-03-11 13:10:45 -0500

    add two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    tmp_list = [0, 0]
+    tmp_list2= [0, 0]
+
+    if len(tuple_a) >= 2:
+        tmp_list[0], tmp_list[1] = tuple_a[0], tuple_a[1]
+    elif len(tuple_a) == 1:
+        tmp_list[0] = tuple_a[0]
+    if len(tuple_b) >= 2:
+        tmp_list2[0], tmp_list2[1] = tuple_b[0], tuple_b[1]
+    elif len(tuple_b) == 1:
+        tmp_list2[0] = tuple_b[0]
+
+    for i in range(len(tmp_list)):
+        tmp_list[i] += tmp_list2[i]
+
+
+    return tuple(tmp_list)
