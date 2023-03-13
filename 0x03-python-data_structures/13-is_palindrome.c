a8fe0ee16a2c138c179cb7cecf24ccb720bb6a16
bush-da <danielsevani12@gmail.com>
2023-03-13 15:43:47 -0400

    checks for palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,43 +8,53 @@
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *ch1 = *head;
+	listint_t *ch2 = *head;
+	listint_t *counter = *head;
+	int count, middle, checker, middle2;
+
 	if (!*head)
 		return 1;
 
-	listint_t *ch1 = *head;
-	listint_t *counter = *head;
-	unsigned int count, checker, middle;
-
 	count = 0;
 	checker = 0;
-	while (counter != NULL)
+
+	while (counter->next != NULL)
 	{
 		count++;
 		counter = counter->next;
 	}
 
-	unsigned int arr[count];
-	count--;
+	middle = count / 2;
+	middle2 = middle;
 
-	middle = (count) / 2;
-
-	while (ch1 != NULL)
+	while (checker <= middle2)
 	{
-		arr[checker] = ch1->n;
 		checker++;
-		ch1 = ch1->next;
+		ch2 = ch2->next;
 	}
+
 	checker = 0;
 
-	while ((checker <= middle) && (middle <= count))
+	while (ch2 != NULL)
 	{
-		if (arr[checker] == arr[count])
+		while (checker < middle)
 		{
-			count--;
 			checker++;
+			ch1 = ch1->next;
+		}
+
+		if (ch2->n == ch1->n)
+		{
+			middle--;
+			checker = 0;
+			ch2 = ch2->next;
+			ch1 = *head;
 		}
 		else
+		{
 			return 0;
+		}
 	}
 
 	return 1;
