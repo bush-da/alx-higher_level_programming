64dc65fdad5594eead576a450f8bbc09e34a0a9a
bush-da <danielsevani12@gmail.com>
2023-03-14 07:15:12 -0400

    pycode style correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
 
     tmp_list = [0, 0]
-    tmp_list2= [0, 0]
+    tmp_list2 = [0, 0]
 
     if len(tuple_a) >= 2:
         tmp_list[0] = tuple_a[0]
@@ -19,7 +19,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 1:
         tmp_list2[0] = tuple_b[0]
 
-    for i in range(len(tmp_list)):
+    for i in range(2):
         tmp_list[i] += tmp_list2[i]
 
     tmp_list = tuple(tmp_list)
