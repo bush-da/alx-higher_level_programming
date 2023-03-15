d3b5c091551d923e7a98e1722c0c382ed9054cc2
bush-da <danielsevani12@gmail.com>
2023-03-15 13:45:19 -0400

    square each element of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    x = []
-    for i in range(len(matrix)):
-        x.append([])
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            y = matrix[i][j] ** 2
-            x[i].append(y)
-    return x
+    return list(map(lambda x: list(map(lambda y: (y ** 2), x)), matrix))
