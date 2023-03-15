ce0fd217be13f55e3d82319b24f9faa015d5c1ed
bush-da <danielsevani12@gmail.com>
2023-03-15 14:26:54 -0400

    add more condition to check

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     return max(a_dictionary)
