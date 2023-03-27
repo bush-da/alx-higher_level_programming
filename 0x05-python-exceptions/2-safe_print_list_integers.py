654c7b6fae3f6df7652a68541400200fc779aca3
bush-da <danielsevani12@gmail.com>
2023-03-27 17:47:34 -0400

    function that prints the first x elements of a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except IndexError:
-            raise IndexError
-        except:
+        except (TypeError, ValueError):
             continue
     print("")
     return (count)
