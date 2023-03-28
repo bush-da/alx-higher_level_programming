fcafff018b7ef74677f6f2591364223b30a4bd2e
bush-da <danielsevani12@gmail.com>
2023-03-28 16:22:04 -0400

    add area method to Square class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """class method that find the area of square
 
