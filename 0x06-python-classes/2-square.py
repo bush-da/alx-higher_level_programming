33b31a69e78da8e8a34e4de088c80032bb9562c0
bush-da <danielsevani12@gmail.com>
2023-03-28 16:12:37 -0400

    redefine a class square with some conditions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a Square"""
+    def __init__(self, size=0):
+
+        """Initialize a new Square
+
+        Args:
+            size: the size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
