f44745744114df1dc0ec2f75d115aaf5b8e1ec90
bush-da <danielsevani12@gmail.com>
2023-03-29 05:40:35 -0400

    add position method to Square class make it print using position spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,8 @@ class Square:
             print("")
             return
 
-        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__position[1]):
+            print("")
         for i in range(0, self.__size):
             for k in range(0, self.__position[0]):
                 print(" ", end="")
