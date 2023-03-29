b1f0611cc5aade47adc24a35cd73d5726d0f6f2d
bush-da <danielsevani12@gmail.com>
2023-03-29 05:33:05 -0400

    add position method to Square class make it print using position spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,8 +46,8 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(i, int) for i in value)
-                or not all(i >= 0 for i in value)):
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -57,9 +57,7 @@ class Square:
             print("")
             return
 
-        for i in range(0, self.__position[1]):
-            print("")
-
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             for k in range(0, self.__position[0]):
                 print(" ", end="")
