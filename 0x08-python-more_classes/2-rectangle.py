aee8da997e4a444920fd017ec901fe3f7d59c070
bush-da <danielsevani12@gmail.com>
2023-04-03 03:41:36 -0700

    add method to Class Retangle that computes area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,11 +19,15 @@ class Rectangle:
 
     def area(self):
         """ return area of rectangle """
+        if (self.__width == 0) or (self.__height == 0):
+            return (0)
         return (self.__width * self.__height)
 
     def perimeter(self):
         """ return the perimeter of the rectangle"""
-        return (self.__width * 2 + self.__height * 2)
+        if (self.__width == 0) or (self.__height == 0):
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     @property
     def width(self):
