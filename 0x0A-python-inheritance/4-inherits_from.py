ce7e9eba6ea9f4bb4df0330238ae0032d75a9b70
bush-da <danielsevani12@gmail.com>
2023-04-10 14:17:33 -0700

    checks for object is an instance of a class that inherited from specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,16 @@ False if it is not """
 
 
 def inherits_from(obj, a_class):
-    """ check for instance of not """
-    return issubclass(type(obj), a_class)
+     """
+     Function to check if an object is an instance of a class that inherited
+     (directly or indirectly) from the specified class.
+
+     Args:
+     obj: An object to check.
+     a_class: A class to check if the object is an instance of.
+
+     Returns:
+     True if the object is an instance of a class that inherited (directly
+     or indirectly) from the specified class; False otherwise.
+     """
+     return issubclass(type(obj), a_class) and type(obj) != a_class
