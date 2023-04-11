7585b97fb5b59e9eb0ad6959381576befa6bfed6
bush-da <danielsevani12@gmail.com>
2023-04-11 12:40:06 -0700

    reads a text file and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ define function that read text file """
-import sys
 
 
 def read_file(filename=""):
     """ reads a text file from filename """
     with open(filename, 'r') as f:
-        sys.stdout.write(f.read())
+        print(f.read(), end="")
