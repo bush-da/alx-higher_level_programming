8764b4ada0a30e86416ae3419f8999cd72834d73
bush-da <danielsevani12@gmail.com>
2023-04-12 05:33:02 -0700

    script that adds all arguments to a python list and then save them to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,18 +3,20 @@
 and save them to a file """
 import sys
 import os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
-check = './add_item.json'
-if os.path.isfile(check):
-    my_list = load_from_json_file(filename)
-    args = len(sys.argv)
-    for i in range(1, args):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, filename)
-else:
-    with open(filename, 'a') as what:
-        what.write('[]')
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    filename = "add_item.json"
+    check = './add_item.json'
+    if os.path.isfile(check):
+        my_list = load_from_json_file(filename)
+        args = len(sys.argv)
+        for i in range(1, args):
+            my_list.append(sys.argv[i])
+        save_to_json_file(my_list, filename)
+    else:
+        with open(filename, 'a') as what:
+            what.write('[]')
