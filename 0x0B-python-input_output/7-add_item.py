9dca803d99a9bc23520724e3373456c45252be8d
bush-da <danielsevani12@gmail.com>
2023-04-12 05:39:38 -0700

    use try and except to check if json file exist or not

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,21 +2,19 @@
 """ Defines a module that adds all arguments to a python list
 and save them to a file """
 import sys
-import os
 
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+
     filename = "add_item.json"
-    check = './add_item.json'
-    if os.path.isfile(check):
+    try:
         my_list = load_from_json_file(filename)
-        args = len(sys.argv)
-        for i in range(1, args):
+    except:
+        my_list = []
+    args = len(sys.argv)
+    for i in range(1, args):
             my_list.append(sys.argv[i])
-        save_to_json_file(my_list, filename)
-    else:
-        with open(filename, 'a') as what:
-            what.write('[]')
+    save_to_json_file(my_list, filename)
