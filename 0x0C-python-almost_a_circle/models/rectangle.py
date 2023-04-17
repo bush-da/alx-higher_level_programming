861581031313e0f0c16664530711f4258f2cd857
bush-da <danielsevani12@gmail.com>
2023-04-17 14:00:45 -0700

    add validation of all setter method and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,15 @@ class Rectangle(Base):
     """ defines a class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initilize the rectanlge instance when created
+
+        Args:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+            x(int): x coordinate of rectangle
+            y(int): y coordinate of rectangle
+            id(int): identity of rectangle
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -15,7 +24,9 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter/setter for width of the rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -26,7 +37,9 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """getter/setter for height of rectangle"""
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -37,7 +50,9 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """getter/setter for x in rectangle"""
         return (self.__x)
+
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
@@ -45,9 +60,12 @@ class Rectangle(Base):
         if value < 0:
             raise TypeError("x must be >= 0")
         self.__x = value
+
     @property
     def y(self):
+        """getter/setter for y in rectangle"""
         return (self.__y)
+
     @x.setter
     def y(self, value):
         if not isinstance(value, int):
