cc6c2b391e02ea32b967dacda87ddbacf3f92ec1
bush-da <danielsevani12@gmail.com>
2023-04-18 07:08:33 -0700

    override __str__ method to print infor about rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,6 @@ class Rectangle(Base):
                 print("")
         print("")
         return ("")
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
