b12a428628ccf9a45b24cd32a17ca436ef2efb0b
bush-da <danielsevani12@gmail.com>
2023-04-18 10:57:39 -0700

    add public method update that assigns attributes using args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,6 +32,42 @@ class Square(Rectangle):
         self.height = value
         self.width = value
 
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            # d = len(args)
+            # if (d == 1):
+            #     self.id = args[0]
+            # elif (d == 2):
+            #     self.id = args[0]
+            #     self.size = args[1]
+            # elif (d == 3):
+            #     self.id = args[0]
+            #     self.size = args[1]
+            #     self.x = args[2]
+            # elif (d == 4):
+            #     self.id = args[0]
+            #     self.size = args[1]
+            #     self.x = args[2]
+            #     self.y = args[3]
+
+            d = 0
+            for arg in args:
+                if d == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif (d == 1):
+                    self.size = arg
+                elif (d == 2):
+                    self.x = arg
+                elif (d == 3):
+                    self.y = arg
+                d += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
                                                          self.y, self.size)
