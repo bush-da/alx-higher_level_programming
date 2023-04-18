84f1e42a32d96b361a9f831bc6074c679b3338fb
bush-da <danielsevani12@gmail.com>
2023-04-18 11:27:39 -0700

    fix typo error to pass PEP 8 validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -62,7 +62,7 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """ Returns the dictionary representation of a Square"""
-        return {"id":self.id, "size":self.size, "x":self.x, "y":self.y}
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
 
     def __str__(self):
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
