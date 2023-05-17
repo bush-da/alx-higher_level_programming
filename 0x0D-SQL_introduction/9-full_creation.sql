a30594d1436fcb64d6c938691373d9c1ed74225c
bush-da <danielsevani12@gmail.com>
2023-05-17 05:33:30 -0700

    create table and add row to it

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates a table second_table in the database and add
 -- multiple rows
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
        id INT,
        name VARCHAR(256),
        score INT
