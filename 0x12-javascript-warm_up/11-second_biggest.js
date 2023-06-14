29ab575a87090dbb8a8b40b85f1ced3930cb40a9
bush-da <danielsevani12@gmail.com>
2023-06-14 12:47:56 -0700

    fix minor issue

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-let num;
-if (isNaN(args[0]) || isNaN(args[1])) {
+if (args.length < 2) {
   console.log(0);
 } else {
-  num = args[0];
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > num) {
-      num = args[i];
-    }
-  }
-  const index = args.indexOf(num);
-  args.splice(index, 1);
-  num = args[0];
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > num) {
-      num = args[i];
-    }
-  }
-  console.log(num);
+  const number = args.map(Number);
+  const sortedNumber = number.sort((a, b) => b - a);
+  console.log(sortedNumber[1]);
 }
