ec79eadb85980e203412be0cd9a67afe5179d495
bush-da <danielsevani12@gmail.com>
2023-07-19 13:14:50 -0700

    computes the score average of all records

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 --script that compute the score average of all records in the table second_table
-SELECT AVG(`score`) AS `average` FROM second_table;
+SELECT AVG(`score`) AS `average` FROM `second_table`;
