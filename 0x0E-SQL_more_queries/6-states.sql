ada2e63282f6289604d499ed390d0b4cf8795e65
bush-da <danielsevani12@gmail.com>
2023-07-20 13:01:04 -0700

    creates tha database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-       id INT NOT NULL UNIQUE AUTO_INCREMENT,
+       id INT NOT NULL AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY(id)
 );
