c014716d8ea0fa837f1be48131231d03f97f9fbf
bush-da <danielsevani12@gmail.com>
2023-07-20 02:44:57 -0700

    creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- script that creates the MySQL server user user_0d_1.
 CREATE USER
-       IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-      ON *.* TO 'user_0d_1'@'locahost'
-      IDENTIFIED BY 'user_0d_1_pwd';
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
