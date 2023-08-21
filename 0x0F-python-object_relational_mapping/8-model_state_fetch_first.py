2c64eed2c00c303620d99a4bad35e0526c81dd17
bush-da <danielsevani12@gmail.com>
2023-08-21 05:22:37 -0700

    fix minor issue

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).order_by(State.id).first()
-    if (state is None):
-        print("Noting")
+    if state is None:
+        print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
