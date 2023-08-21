5edd2823a3ee07e3d85b4bacb1b8914a3b429eed
bush-da <danielsevani12@gmail.com>
2023-08-21 08:20:27 -0700

    script that prints all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for city, state in session.query(City, State) \
-                       .filter(City.state_id == State.id) \
-                       .order_by(City.id):
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
