e4c0f3f34936bf4b7c41df772344758ddf4252f4
bush-da <danielsevani12@gmail.com>
2023-08-21 02:40:29 -0700

    script that display all values in the states table where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    curr = db.cursor()
+    curr.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in curr.fetchall() if (state[1] == sys.argv[4])]
