5b77b6ddaec7e6140e9a7b9f183e4ce4beac5d06
bush-da <danielsevani12@gmail.com>
2023-09-01 14:06:41 -0700

    script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL
+and displays the body of the response."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if (req.status_code >= 400):
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
