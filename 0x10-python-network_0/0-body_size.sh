1fe5d87241ad46c8aeb4d82588f521e4714ecca3
bush-da <danielsevani12@gmail.com>
2023-09-01 04:52:57 -0700

    script that takes in a URL sends a request and display the size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -4,7 +4,7 @@
 url=$1
 
 # Send request to URL and store response in variable
-response=$(curl -s -o /dev/null -w "%{size_download}" $url)
+response=$(curl -s $url | wc -c)
 
 # Display size of response body in bytes
 echo $response
