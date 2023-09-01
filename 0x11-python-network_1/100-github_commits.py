ffedc767e9e176d70aa6686e986ec33b1253d882
bush-da <danielsevani12@gmail.com>
2023-09-01 14:59:02 -0700

    fix pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[1], sys.argv[2])
+        sys.argv[2], sys.argv[1])
 
     req = requests.get(url)
     commits = req.json()
