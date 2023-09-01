81c89972fa3c8317b063db15dc72ccd6040b3b3c
bush-da <danielsevani12@gmail.com>
2023-09-01 04:56:08 -0700

    script that takes in a URL sends a request and display the size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Take in URL as argument
-url=$1
-
-# Send request to URL and store response in variable
-response=$(curl -s $url | wc -c)
-
-# Display size of response body in bytes
-echo $response
+# script that takes in a URL, sends a request and displays the size
+echo $(curl -s $1 | wc -c)
