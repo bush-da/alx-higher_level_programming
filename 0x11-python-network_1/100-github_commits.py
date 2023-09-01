6d8f4aa3de1530cfcd70f8db389856ef9cf1528e
bush-da <danielsevani12@gmail.com>
2023-09-01 14:57:46 -0700

    fix pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,14 +6,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".\
-        format(sys.argv[1], sys.argv[2])
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2])
 
     req = requests.get(url)
     commits = req.json()
     try:
         for i in range(10):
-            print("{}: {}".format(\
+            print("{}: {}".format(
                 commits[i].get("sha"),
                 commits[i].get("commit").get("author").get("name")))
     except IndexError:
