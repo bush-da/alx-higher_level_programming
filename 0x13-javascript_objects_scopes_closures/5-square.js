2857a36206ad6e3dfcd5a403a42d19e420748af4
bush-da <69421999+bush-da@users.noreply.github.com>
2023-11-12 13:03:39 -0700

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (s) {
-    super(s, s);
-  }
-};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const temp = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    temp.push(list[i]);
-  }
-  return temp;
-};
