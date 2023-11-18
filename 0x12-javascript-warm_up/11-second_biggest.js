346d2ecdb5b08dafa9cccae6dc5d557d0b154879
bush-da <danielsevani12@gmail.com>
2023-11-18 06:13:01 -0700

    script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 let i = 2;
 const myarray = [];
-if (isNaN(process.argv[2]) || isNaN(process.argv[3])) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
   while (!isNaN(process.argv[i])) {
     myarray.push(process.argv[i]);
     i++;
   }
-  console.log(myarray.sort()[myarray.length - 2]);
+  console.log(myarray.sort((a, b) => a - b)[myarray.length - 2]);
 }
+console.log(myarray);
