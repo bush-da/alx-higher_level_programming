c96c6b671267b4475ccd15c7fd6d15983955e131
bush-da <danielsevani12@gmail.com>
2023-11-18 13:36:36 -0700

    function that increments and calls a function

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports.addMeMaybe = function(number, theFunction) {
-    number++;
-    theFunction(number);
-}
+module.exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
