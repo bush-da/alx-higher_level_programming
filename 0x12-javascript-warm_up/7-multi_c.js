3ad6e19c0ab0a61a71b16a48a70a357f7074c63a
bush-da <danielsevani12@gmail.com>
2023-11-18 02:08:11 -0700

    prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (isNaN(parseInt(Number(process.argv[2])))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
