1f4bd7ce1a374b19826af0595c80510ca05ce54e
bush-da <danielsevani12@gmail.com>
2023-11-18 01:53:54 -0700

    prints if the first argument can be converted to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const num = parseInt(Number(process.argv[2]))
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`)
+const num = parseInt(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
