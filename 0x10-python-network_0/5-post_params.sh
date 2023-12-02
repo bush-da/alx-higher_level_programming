a221cbb541d645c32b3c66ba71f6afb5b9367793
bush-da <danielsevani12@gmail.com>
2023-12-02 15:29:14 -0500

    fix minor issue

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST req and set email and subject
-curl -s -X POST -H "email: test@gmail.com" "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email: test@gmail.com" "subject: I will always be here for PLD" "$1"
