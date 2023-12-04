29c16996d4ec791af5423d22c490adf473c1de31
bush-da <danielsevani12@gmail.com>
2023-12-04 15:28:11 -0500

    send req to server

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# makes request to 0.0.0.0:5000/catch_me the causes the server to respond with
+# a message containing You got me!
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
