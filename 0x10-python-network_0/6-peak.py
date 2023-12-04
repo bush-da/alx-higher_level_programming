dfb7ac2efd76158f8cda3a8163f7f961d5651c24
bush-da <danielsevani12@gmail.com>
2023-12-04 04:57:02 -0500

    peak finding algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """ peak find algorithm """
 
+
 def find_peak(list_of_integers):
     """ checks for peak iterating through the list and return if
     current number if greater than from both side """
-    for i in range(1, len(list_of_integers) - 1):
-        if list_of_integers[i - 1] <= list_of_integers[i] >= list_of_integers[
-            i + 1
-        ] and i < len(list_of_integers):
-            return list_of_integers[i]
+    if list_of_integer == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    if size == 2:
+        return max(list_of_integers)
+    mid = len(list_of_integers)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
