b395e4e997df10f9ecf2672adc979f9784e76366
bush-da <danielsevani12@gmail.com>
2023-12-04 04:59:14 -0500

    algorithm to find peak number

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@
 def find_peak(list_of_integers):
     """ checks for peak iterating through the list and return if
     current number if greater than from both side """
-    if list_of_integer == []:
+    if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
@@ -13,7 +13,7 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if size == 2:
         return max(list_of_integers)
-    mid = len(list_of_integers)
+    mid = int(len(list_of_integers) / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
