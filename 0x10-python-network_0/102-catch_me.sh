b289a3c4b23064d493ec4ee5530e6a3b295e9c50
bush-da <danielsevani12@gmail.com>
2023-12-04 15:29:08 -0500

    req to server

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# makes request to 0.0.0.0:5000/catch_me the causes the server to respond with
-# a message containing You got me!
+# makes request to 0.0.0.0:5000/catch_me the causes the server to respond
 curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
