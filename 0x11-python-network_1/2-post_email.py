56598ec0531d55cc508e12cd331db2e06a6c6ed5
bush-da <danielsevani12@gmail.com>
2023-12-07 11:46:13 -0500

    change encoding from utf-8 to ascii

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("utf-8")
+    data = urllib.parse.urlencode(value).encode("ascii")
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(f"Your email is: {response.read().decode('utf-8')}")
