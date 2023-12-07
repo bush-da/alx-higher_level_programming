7acba417c5b9aaa7e93ffb752f526e669db2fe09
bush-da <danielsevani12@gmail.com>
2023-12-07 12:54:51 -0500

    fix minor issue

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,8 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email': sys.argv[2]}
+    value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8')
+        print(response.read().decode("utf-8"))
