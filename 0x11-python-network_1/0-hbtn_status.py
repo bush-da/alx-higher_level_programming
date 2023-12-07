9233be1916924d8116de433ab3d047ab6fb230fe
bush-da <danielsevani12@gmail.com>
2023-12-07 07:02:47 -0500

    fetch a content from website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,11 @@
 import urllib.request
 
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 with urllib.request.urlopen(req) as response:
     res = response.read()
 
-print(f"Body response:\n\t- type: {type(res)}\n\t- content: {res}\n\t- utf8 content: {res.decode('utf-8')}")
+print(
+    f"Body response:\n\t- type: {type(res)}\n\t-\
+ content: {res}\n\t- utf8 content: {res.decode('utf-8')}"
+)
