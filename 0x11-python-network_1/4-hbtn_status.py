b8f4b81743992a3a8d2df0ea5f64d79ff347b7ea
bush-da <danielsevani12@gmail.com>
2023-12-07 13:51:39 -0500

    fetch url using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,5 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get("https://alx-intranet.hbtn.io/status")
-    print(
-        f"Body response:\n\t- type: \
-    {type(req.text)}\n\t- content: {req.text}"
-    )
+    print(f"Body response:\n\t- type: {type(req.text)}\n\t-\
+    content: {req.text}")
