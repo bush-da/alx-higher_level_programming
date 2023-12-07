598d634409ea8289fcb6aa6861dfbbf2cadec238
bush-da <danielsevani12@gmail.com>
2023-12-07 06:48:35 -0500

    fetch a content from website

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    res = response.read()
+
+print(f"Body response:\n\t- type: {type(res)}\n\t- content: {res}\n\t- utf9 content: {res.decode('utf-8')}")
