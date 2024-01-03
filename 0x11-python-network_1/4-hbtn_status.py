d559548f85c246c662adb3d985e686fe1433fa62
bush-da <69421999+bush-da@users.noreply.github.com>
2024-01-03 00:27:13 -0500

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
-import requests
-
-if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print(f"\t- type: {type(req.text)}\n\t- content: {req.text}")
