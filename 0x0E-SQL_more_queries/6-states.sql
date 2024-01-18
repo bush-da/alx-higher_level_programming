9a1d875d7c5eb8b07f5ab36547e4b8060a84c6f8
Daniel Tujuma <danielsevani12@gmail.com>
2024-01-18 15:40:30 -0500

    create database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- creates the table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256)
+       )
