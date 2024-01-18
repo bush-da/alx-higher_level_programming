65820aa7aded1f7296096f9b211a367dfa04fa82
Daniel Tujuma <danielsevani12@gmail.com>
2024-01-18 18:10:39 -0500

    fix minor issue

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.id
+FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
