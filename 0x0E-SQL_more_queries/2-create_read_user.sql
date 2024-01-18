b6253fcc690a7ca09febc4e829e7399d72e0c9a6
Daniel Tujuma <danielsevani12@gmail.com>
2024-01-18 14:55:38 -0500

    fix minor issue

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create or update user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
