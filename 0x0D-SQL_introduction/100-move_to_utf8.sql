309cd12dca56292a85f797f85153d11ffc74cae3
Daniel Tujuma <danielsevani12@gmail.com>
2024-01-18 06:54:56 -0500

    fix minor issue

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- convert the database to UTF8
-ALTER TABLE hbtn_0c_.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
 
