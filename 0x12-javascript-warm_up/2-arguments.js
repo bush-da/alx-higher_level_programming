292d595ad8ffddb1c5e45ec774c5988365e9e3a3
Daniel Tujuma <danielsevani12@gmail.com>
2024-02-12 05:23:51 -0500

    fix minor issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument Found' : 'Arguments found');
+console.log(count === 2
+  ? 'No argument'
+  : count === 3 ? 'Argument Found' : 'Arguments found');
