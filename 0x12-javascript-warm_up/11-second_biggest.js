26440adf838b4d35edb84d18aefef8514b68cd2e
Daniel Tujuma <danielsevani12@gmail.com>
2024-02-12 06:49:30 -0500

    fix minor issue

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const myargs = process.argv;
 if (myargs.length > 3) {
   const args = myargs.map(Number)
     .slice(2, myargs.length)
-    .sort();
+    .sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 } else {
   console.log(0);
