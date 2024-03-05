28684b962df5653fd9a9a63af3667b43aefe647e
Daniel Tujuma <danielsevani12@gmail.com>
2024-03-05 16:20:36 -0500

    display body of url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
+        body = res.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
