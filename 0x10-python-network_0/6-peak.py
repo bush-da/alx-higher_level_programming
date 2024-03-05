1b0e7a093fe647a1c013f7ecb005d2cbbfc84701
Daniel Tujuma <danielsevani12@gmail.com>
2024-03-05 09:26:40 -0500

    fix minor issue

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,7 +14,7 @@ def find_peak(list_of_integers):
     while start < end:
         mid = (start + end) // 2
         if (list_of_integers[mid] < list_of_integers[mid + 1]):
-            start = mid
+            start = mid + 1
         else:
             end = mid
     return list_of_integers[start]
