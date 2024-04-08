250877ba9e734cb35848072fbe224966fcb956a2
Daniel Tujuma <danielsevani12@gmail.com>
2024-04-08 18:17:53 -0400

    fix minor issue

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fac = (a) => {
+function fac (a) {
   if (a === 1) {
     return 1;
   }
-  return (a * fac(a - 1));
-};
+  return a * fac(a - 1);
+}
 if (!process.argv[2]) {
   console.log(1);
 } else {
