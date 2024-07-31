f290fd09c57e5cc4c7e294a7673a3b109a5d0be2
Daniel Tujuma <danielsevani12@gmail.com>
2024-07-31 15:30:44 -0400

    print all characters of star wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', response.statusCode);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).characters;
+    films.forEach((character) => {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log('Error:', response.statusCode);
+        } else if (response.statusCode === 200) {
+          const name = JSON.parse(body).name;
+          console.log(name);
+        } else {
+          console.error('Failed to fetch the webpage', response.statusCode);
+        }
+      });
+    });
+  } else {
+    console.error('Failed to fetch the webpage', response.statusCode);
+  }
+});
