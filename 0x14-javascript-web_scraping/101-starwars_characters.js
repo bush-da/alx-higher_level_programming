e39383c2d4f69960789fe26369083ad98a9e786d
Daniel Tujuma <danielsevani12@gmail.com>
2024-07-31 16:27:33 -0400

    fix minor issue

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,22 +5,38 @@ const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', response.statusCode);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).characters;
-    for (let i = 0; i < films.length; i++) {
-      request.get(films[i], (error, response, body) => {
-        if (error) {
-          console.log('Error:', response.statusCode);
-        } else if (response.statusCode === 200) {
-          const name = JSON.parse(body).name;
-          console.log(name);
-        } else {
-          console.error('Failed to fetch the webpage', response.statusCode);
-        }
-      });
-    }
-  } else {
-    console.error('Failed to fetch the webpage', response.statusCode);
+    console.error('Error:', error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch film data:', response.statusCode);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+  const characterNames = new Array(characters.length);
+  let completedRequests = 0;
+
+  characters.forEach((characterUrl, index) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      if (response.statusCode === 200) {
+        characterNames[index] = JSON.parse(body).name;
+      } else {
+        console.error('Failed to fetch character data:', response.statusCode);
+      }
+
+      completedRequests++;
+      if (completedRequests === characters.length) {
+        characterNames.forEach(name => {
+          if (name) console.log(name);
+        });
+      }
+    });
+  });
 });
