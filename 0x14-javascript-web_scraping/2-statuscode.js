5f9751e97c7cc72164e76572a7babbdd448d0ce3
Daniel Tujuma <danielsevani12@gmail.com>
2024-07-31 04:41:46 -0400

    display status code of GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.log('Error: ' + error);
+  } else {
+    console.log('Code: ' + response.statusCode);
+  }
+});
